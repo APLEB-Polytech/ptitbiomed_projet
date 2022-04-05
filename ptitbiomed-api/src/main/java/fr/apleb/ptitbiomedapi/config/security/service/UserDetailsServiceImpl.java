7026e0695f9815a0aa7e15775c0ced0fa29d24de
baudu <bastien.audu@gmail.com>
2022-04-05 13:40:56 +0200

    jwt back

---
+++ ptitbiomed-api/src/main/java/fr/apleb/ptitbiomedapi/config/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package fr.apleb.ptitbiomedapi.config.security.service;
+
+import fr.apleb.ptitbiomedapi.model.user.User;
+import fr.apleb.ptitbiomedapi.repository.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    final
+    UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+        return UserDetailsImpl.build(user);
+    }
+}
