c5cdd73837127d594bad09f4d1118c464a3009e4
baudu <bastien.audu@gmail.com>
2022-07-10 22:30:02 +0200

    tests

--- ptitbiomed-api/src/test/java/fr/apleb/ptitbiomedapi/PtitbiomedApiApplicationTests.java
+++ ptitbiomed-api/src/test/java/fr/apleb/ptitbiomedapi/PtitbiomedApiApplicationTests.java
@@ -7,9 +7,7 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 class PtitbiomedApiApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
