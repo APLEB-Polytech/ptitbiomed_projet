052a5db307cdcc10653fa7a8b8fe8fd01d5a708d
baudu <bastien.audu@gmail.com>
2022-08-03 22:48:42 +0200

    redirect

--- ptitbiomed-api/src/main/java/fr/apleb/ptitbiomedapi/controller/BaseController.java
+++ ptitbiomed-api/src/main/java/fr/apleb/ptitbiomedapi/controller/BaseController.java
@@ -1,10 +1,11 @@
 package fr.apleb.ptitbiomedapi.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class BaseController {
+public class BaseController implements ErrorController {
 	@GetMapping(value = "/error")
 	public String error() {
 		return "forward:/index.html";
