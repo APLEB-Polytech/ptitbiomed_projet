674e2c41cfae0bb685eef86781d1f2a4805a62c5
baudu <bastien.audu@gmail.com>
2022-08-21 19:49:51 +0200

    merge with master

--- ptitbiomed-api/src/main/java/fr/apleb/ptitbiomedapi/controller/MenuController.java
+++ ptitbiomed-api/src/main/java/fr/apleb/ptitbiomedapi/controller/MenuController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/menu")
 public class MenuController {
-    private final static Logger logger = LoggerFactory.getLogger(RoleController.class);
+    private final static Logger logger = LoggerFactory.getLogger(MenuController.class);
     private final MenuService menuService;
 
     public MenuController(MenuService menuService) {
