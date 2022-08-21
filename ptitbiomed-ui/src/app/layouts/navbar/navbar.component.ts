777ceac45f48d45392e04f13d735433207de33ae
baudu <bastien.audu@gmail.com>
2022-08-21 21:08:34 +0200

    Navbar Admin

--- ptitbiomed-ui/src/app/layouts/navbar/navbar.component.ts
+++ ptitbiomed-ui/src/app/layouts/navbar/navbar.component.ts
@@ -13,6 +13,63 @@ export class NavbarComponent implements OnInit {
 
   @Input()
   menuItems: IMenu[] | undefined;
+  menuItemsAdmin: IMenu[] = [
+    {
+      id: 0,
+      label: 'Administration',
+      link: '',
+      rank: 0,
+      submenuas: [
+        {
+          id: 0,
+          label: 'Utilisateurs',
+          link: '/admin/user',
+          submenubs: []
+        },
+        {
+          id: 1,
+          label: 'Panel',
+          link: '/admin/panel',
+          submenubs: []
+        },
+        {
+          id: 2,
+          label: 'Medias',
+          link: '',
+          submenubs: [
+            {
+              id: 1,
+              label: 'Liste des medias',
+              link: '/medias'
+            },
+            {
+              id: 2,
+              label: 'Upload d\'un media',
+              link: '/medias/upload'
+            }
+          ]
+        },
+        {
+          id: 3,
+          label: 'Articles',
+          link: '',
+          submenubs: [
+            {
+              id: 1,
+              label: 'Liste des articles',
+              link: '/article'
+            },
+            {
+              id: 2,
+              label: 'Upload d\'un article',
+              link: '/article/new'
+            }
+          ]
+        }
+      ]
+    },
+
+  ]
 
   constructor(public userService: UserService, public menuService: MenuService, private router: Router) {
   }
