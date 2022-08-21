5f3ca509da888881517f0757270ffdb888e9e316
baudu <bastien.audu@gmail.com>
2022-08-21 14:08:54 +0200

    merge master

--- ptitbiomed-ui/src/app/articles/articles-routing.module.ts
+++ ptitbiomed-ui/src/app/articles/articles-routing.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {AdminGuard} from "../shared/guard/admin.guard";
 import {ArticleEditorComponent} from "./article-editor/article-editor.component";
 import {ListeArticlesComponent} from "./liste-articles/liste-articles.component";
+import {ConnectedGuard} from "../shared/guard/connected.guard";
 
 const routes: Routes = [
-	{path: '', component: ListeArticlesComponent, canActivate: [AdminGuard]},
-	{path: 'new', component: ArticleEditorComponent, canActivate: [AdminGuard]},
+  {path: '', component: ListeArticlesComponent, canActivate: [ConnectedGuard]},
+  {path: 'new', component: ArticleEditorComponent, canActivate: [ConnectedGuard]},
 ];
 
 @NgModule({
