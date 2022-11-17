35ec733842915621322e6991b4224e71dbca5e3b
Bastien Audu <bastien.audu@gmail.com>
2022-11-17 18:44:11 +0100

    Suppression des classes sous-menus #85

--- ptitbiomed-api/src/main/java/fr/apleb/ptitbiomedapi/service/ArticleService.java
+++ ptitbiomed-api/src/main/java/fr/apleb/ptitbiomedapi/service/ArticleService.java
@@ -2,14 +2,9 @@ package fr.apleb.ptitbiomedapi.service;
 
 import fr.apleb.ptitbiomedapi.exception.NotFoundException;
 import fr.apleb.ptitbiomedapi.model.article.*;
-import fr.apleb.ptitbiomedapi.model.menu.Menu;
-import fr.apleb.ptitbiomedapi.model.menu.Submenua;
-import fr.apleb.ptitbiomedapi.model.menu.Submenub;
 import fr.apleb.ptitbiomedapi.repository.ArticleRepository;
 import fr.apleb.ptitbiomedapi.repository.menu.MenuRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -40,7 +35,6 @@ public class ArticleService {
 		return articleRepository.findByTitle("accueil").map(ArticleReadDto::new);
 	}
 
-	@Transactional(isolation = Isolation.READ_UNCOMMITTED)
 	public void createNew(ArticleCreationDto articleCreation) {
 		final LocalDateTime now = LocalDateTime.now();
 
@@ -50,58 +44,21 @@ public class ArticleService {
 		article.setAuthor(articleCreation.author());
 		article.setTitle(articleCreation.title());
 		article.setHtml(articleCreation.html());
-		final Article article1 = articleRepository.save(article);
-		this.processMenuArticle(new ArticleUpdateDto(null, articleCreation.title(), null, article1.getUuid(), articleCreation.menuArticle()));
+		this.articleRepository.save(article);
 	}
 
-	@Transactional(isolation = Isolation.READ_UNCOMMITTED)
 	public void updateArticle(ArticleUpdateDto articleReadDto) {
 		Article article = this.articleRepository.findById(articleReadDto.uuid()).orElseThrow(NotFoundException::new);
 		article.setTitle(articleReadDto.title());
 		article.setAuthor(articleReadDto.author());
 		article.setHtml(articleReadDto.html());
 		article.setUpdateTime(LocalDateTime.now());
-		this.processMenuArticle(articleReadDto);
 		this.articleRepository.save(article);
 	}
 
 	public void deleteArticle(UUID uuid) {
 		if (!this.articleRepository.existsById(uuid)) throw new NotFoundException();
 		this.articleRepository.deleteById(uuid);
-	}
-
-	/**
-	 * Ajoute un article au menu si son objet menuArticleDto n'est pas null
-	 */
-	private void processMenuArticle(EditArticleDTO editArticleDTO) {
-		/*
-		TODO: Check
-		if (editArticleDTO.menuArticle() == null) {
-			return;
-		}
-		final MenuArticleDto menuArticleDto = editArticleDTO.menuArticle();
-		if (menuArticleDto.idSousMenu() != null) {
-			Submenua parentA = this.submenuaRepository.findByIdAndIdParent(menuArticleDto.idSousMenu(), menuArticleDto.idMenu());
-			Submenub submenub = new Submenub();
-			submenub.setIdParent(parentA.getId());
-			submenub.setLabel(editArticleDTO.title());
-			submenub.setIdArticle(editArticleDTO.uuid().toString());
-			this.submenubRepository.save(submenub);
-			return;
-		}
-		if (menuArticleDto.idMenu() != null) {
-			Submenua submenua = new Submenua();
-			submenua.setIdParent(menuArticleDto.idMenu());
-			submenua.setLabel(editArticleDTO.title());
-			submenua.setIdArticle(editArticleDTO.uuid().toString());
-			this.submenuaRepository.save(submenua);
-			return;
-		}
-		Menu menu = new Menu();
-		menu.setLabel(editArticleDTO.title());
-		menu.setIdArticle(editArticleDTO.uuid().toString());
-		this.menuRepository.save(menu);
-		*/
 	}
 
 
