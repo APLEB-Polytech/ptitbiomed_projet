7dc315e7f6f366bec1f555344b2afbfff5f85148
Alexis Giust <alx.giust@gmail.com>
2023-03-25 17:27:47 +0100

    Fix UserService unit tests

---
+++ ptitbiomed-api/src/test/java/fr/apleb/ptitbiomedapi/testutils/UnitTest.java
@@ -0,0 +1,10 @@
+package fr.apleb.ptitbiomedapi.testutils;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(ReplaceUnderscores.class)
+public interface UnitTest {}
