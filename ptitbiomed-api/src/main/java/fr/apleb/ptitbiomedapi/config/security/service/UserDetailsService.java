3494d9c4326487dcb83828f3409a05daae91c25a
Bastien Audu <bastien.audu@gmail.com>
2023-06-09 18:20:53 +0200

    remove useless

--- ptitbiomed-api/src/main/java/fr/apleb/ptitbiomedapi/config/security/service/UserDetailsService.java
+++
@@ -1,5 +0,0 @@
-package fr.apleb.ptitbiomedapi.config.security.service;
-
-//public interface UserDetailsService extends org.springframework.security.core.userdetails.UserDetailsService {
-//    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-//}
