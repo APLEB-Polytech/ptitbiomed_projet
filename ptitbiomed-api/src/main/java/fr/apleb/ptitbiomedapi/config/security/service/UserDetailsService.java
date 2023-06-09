9edb60e6b1511e25349e3480658d9f9743895a61
Bastien Audu <bastien.audu@gmail.com>
2023-06-09 18:18:53 +0200

    remove lasts legacy

--- ptitbiomed-api/src/main/java/fr/apleb/ptitbiomedapi/config/security/service/UserDetailsService.java
+++ ptitbiomed-api/src/main/java/fr/apleb/ptitbiomedapi/config/security/service/UserDetailsService.java
@@ -1,8 +1,5 @@
 package fr.apleb.ptitbiomedapi.config.security.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface UserDetailsService extends org.springframework.security.core.userdetails.UserDetailsService {
-    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-}
+//public interface UserDetailsService extends org.springframework.security.core.userdetails.UserDetailsService {
+//    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+//}
